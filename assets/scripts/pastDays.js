c6a3a06c5cba51e0f2046cbaccdd18c243e054bf
GoudaMorganing <35888974+GoudaMorganing@users.noreply.github.com>
2025-03-16 17:56:10 +0800

    Added checking of completed past puzzles

--- assets/scripts/pastDays.js
+++ assets/scripts/pastDays.js
@@ -11,10 +11,33 @@ var puzzleNum = Difference_In_Days;
 
 for (let i = 1; i < puzzleNum; i++) {
   let outerNode = document.createElement("a");
-  outerNode.innerHTML = `<a href="solo_game.html">
+
+  if (localStorage.getItem("alltimeResults")) {
+    let alltimeResults = JSON.parse(localStorage.getItem("alltimeResults"));
+
+    let historyCheck = alltimeResults.history.some(
+      (el) => parseInt(el.puzzle) === i
+    );
+
+    if (historyCheck) {
+      outerNode.innerHTML = `
+          <div class="past-played-btn boxed" id="puzzle-${i}">
+          <i class="fa-solid fa-check"></i>
+          </div>`;
+    } else {
+      outerNode.innerHTML = `<a href="solo_game.html">
               <div class="past-btn boxed" id="puzzle-${i}">
               ${i}
               </div>
           </a>`;
+    }
+  } else {
+    outerNode.innerHTML = `<a href="solo_game.html">
+              <div class="past-btn boxed" id="puzzle-${i}">
+              ${i}
+              </div>
+          </a>`;
+  }
+
   PAST_PUZZLES.appendChild(outerNode);
 }
