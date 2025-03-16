2d8993da695d69a94ecfd704d76ce4e0c87a768d
GoudaMorganing <35888974+GoudaMorganing@users.noreply.github.com>
2025-03-16 17:44:17 +0800

    Removed comments

--- assets/scripts/pastDays.js
+++ assets/scripts/pastDays.js
@@ -8,7 +8,6 @@ const PAST_PUZZLES = document.querySelector("#past-puzzles");
 var diffTime = new Date(today).getTime() - new Date("1/12/2024").getTime();
 var Difference_In_Days = Math.round(diffTime / (1000 * 3600 * 24));
 var puzzleNum = Difference_In_Days;
-// sessionStorage.setItem("puzzleNum", puzzleNum);
 
 for (let i = 1; i < puzzleNum; i++) {
   let outerNode = document.createElement("a");
